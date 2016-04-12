51e4e74366354c4716de25dd307616aba43f805a
Florian <laforest.florian@gmail.com>
2016-04-12 12:24:24 +0200

    Ajout des infos sur le modèle Film (version lite)

--- app/Film.php
+++ app/Film.php
@@ -8,13 +8,15 @@ use Illuminate\Database\Eloquent\Model;
  * @SWG\Definition(
  *     required={"id_film", "titre", "resum", "date_debut_affiche", "date_fin_affiche", "duree_minutes", "annee_production"},
  *     @SWG\Xml(name="Film"),
- *     @SWG\Property(property="id_film", format="int64", type="integer"),
- *     @SWG\Property(property="titre", format="string", type="string"),
- *     @SWG\Property(property="resum", format="string", type="string"),
- *     @SWG\Property(property="date_debut_affiche", format="date", type="string"),
- *     @SWG\Property(property="date_fin_affiche", format="date", type="string"),
- *     @SWG\Property(property="duree_minutes", format="int", type="integer"),
- *     @SWG\Property(property="annee_production", format="int", type="integer")
+ *     @SWG\Property(property="id_film", format="int64", type="integer", default=42),
+ *     @SWG\Property(property="id_genre", format="int64", type="integer", default=1),
+ *     @SWG\Property(property="id_distributeur", format="int64", type="integer", default=1),
+ *     @SWG\Property(property="titre", format="string", type="string", default="Deadpool"),
+ *     @SWG\Property(property="resum", format="string", type="string", default="Deadpool, est l'anti-héros le plus atypique de l'univers Marvel. A l'origine, il s'appelle Wade Wilson : un ancien militaire des Forces Spéciales devenu mercenaire. Après avoir subi une expérimentation hors norme qui va accélérer ses pouvoirs de guérison, il va devenir Deadpool. Armé de ses nouvelles capacités et d'un humour noir survolté, Deadpool va traquer l'homme qui a bien failli anéantir sa vie."),
+ *     @SWG\Property(property="date_debut_affiche", format="date", type="string", default="2016-02-10"),
+ *     @SWG\Property(property="date_fin_affiche", format="date", type="string", default="2016-04-30"),
+ *     @SWG\Property(property="duree_minutes", format="int", type="integer", default=108),
+ *     @SWG\Property(property="annee_production", format="int", type="integer", default=2015)
  * )
  */
 
