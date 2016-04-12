1a9de1cf3c16638bdec754208ea93a84666ba310
Florian <laforest.florian@gmail.com>
2016-04-12 11:54:11 +0200

    Ajout des infos sur le modèle Film

--- app/Film.php
+++ app/Film.php
@@ -13,4 +13,46 @@ class Film extends Model
 {
     public $primaryKey = "id_film";
     public $timestamps = false;
+
+    /**
+     * @SWG\Property()
+     * @var int
+     */
+    public $id_film;
+
+    /**
+     * @SWG\Property()
+     * @var string
+     */
+    public $titre;
+
+    /**
+     * @SWG\Property()
+     * @var string
+     */
+    public $resum;
+
+    /**
+     * @SWG\Property()
+     * @var date
+     */
+    public $date_debut_affiche;
+
+    /**
+     * @SWG\Property()
+     * @var date
+     */
+    public $date_fin_affiche;
+
+    /**
+     * @SWG\Property()
+     * @var int
+     */
+    public $duree_minutes;
+
+    /**
+     * @SWG\Property()
+     * @var int
+     */
+    public $annee_production;
 }
