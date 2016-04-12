7b4b39d8464d70c290913c80761e479ca0bbba32
Florian <laforest.florian@gmail.com>
2016-04-12 12:17:46 +0200

    Ajout des infos sur le modèle Film (version lite)

--- app/Film.php
+++ app/Film.php
@@ -5,54 +5,21 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @SWG\Definition(required={"id_film"}, @SWG\Xml(name="Film"))
+ * @SWG\Definition(
+ *     required={"id_film", "titre", "resum", "date_debut_affiche", "date_fin_affiche", "duree_minutes", "annee_production"},
+ *     @SWG\Xml(name="Film"),
+ *     @SWG\Property(property="id_film", format="int64", type="integer"),
+ *     @SWG\Property(property="titre", format="string", type="string"),
+ *     @SWG\Property(property="resum", format="string", type="string"),
+ *     @SWG\Property(property="date_debut_affiche", format="date", type="string"),
+ *     @SWG\Property(property="date_fin_affiche", format="date", type="string"),
+ *     @SWG\Property(property="duree_minutes", format="int", type="integer"),
+ *     @SWG\Property(property="annee_production", format="int", type="integer")
+ * )
  */
-
 
 class Film extends Model
 {
     public $primaryKey = "id_film";
     public $timestamps = false;
-
-    /**
-     * @SWG\Property()
-     * @var int
-     */
-    public $id_film;
-
-    /**
-     * @SWG\Property()
-     * @var string
-     */
-    public $titre;
-
-    /**
-     * @SWG\Property()
-     * @var string
-     */
-    public $resum;
-
-    /**
-     * @SWG\Property()
-     * @var date
-     */
-    public $date_debut_affiche;
-
-    /**
-     * @SWG\Property()
-     * @var date
-     */
-    public $date_fin_affiche;
-
-    /**
-     * @SWG\Property()
-     * @var int
-     */
-    public $duree_minutes;
-
-    /**
-     * @SWG\Property()
-     * @var int
-     */
-    public $annee_production;
 }
