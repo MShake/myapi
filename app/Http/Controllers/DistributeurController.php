2e8cdb460def6252d159b52b8a72cdd105e0abfd
Kalimerre <kalimerre@gmail.com>
2016-04-12 15:06:15 +0200

    Add distributorController

--- app/Http/Controllers/DistributeurController.php
+++ app/Http/Controllers/DistributeurController.php
@@ -14,6 +14,22 @@ class DistributeurController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *     path="/distributeur",
+     *     summary="Display a listing of distributors.",
+     *     tags={"distributeur"},
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Distributeur")
+     *          ),
+     *     ),
+     *  )
+     */
     public function index()
     {
         $distributeurs = Distributeur::all();
@@ -37,9 +53,45 @@ class DistributeurController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *     path="/distributeur/{id_distributeur}",
+     *     summary="Find distributor by ID",
+     *     description="Returns a single distributor",
+     *     operationId="getDistributorById",
+     *     tags={"distributeur"},
+     *     consumes={"application/x-www-form-urlencoded"},
+     *     @SWG\Parameter(
+     *         description="ID of distributor to return",
+     *         in="path",
+     *         name="id_distributeur",
+     *         required=true,
+     *         type="integer",
+     *         format="int64"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     ),
+     *     @SWG\Response(
+     *         response="404",
+     *         description="Distributor not found"
+     *     )
+     * )
+     */
     public function show($id)
     {
-        //
+        $distributeur = Distributeur::find($id);
+
+        if (empty($distributeur)) {
+            return response()->json(
+                ['error' => 'this distributor does not exist'],
+                404);
+        }
+
+
+        return $distributeur;
     }
 
     /**
@@ -60,8 +112,44 @@ class DistributeurController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Delete(
+     *     path="/distributeur/{id_distributeur}",
+     *     summary="Delete a distributor",
+     *     description="Delete a distributor through an ID",
+     *     operationId="deleteDistributeur",
+     *     tags={"distributeur"},
+     *     @SWG\Parameter(
+     *         description="Distributeur ID to delete",
+     *         in="path",
+     *         name="id_distributeur",
+     *         required=true,
+     *         type="integer",
+     *         format="int64"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Distributor deleted"
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Invalid distributor value"
+     *     )
+     *
+     * )
+     */
+
     public function destroy($id)
     {
-        //
+        $distributeur = Distributeur::find($id);
+
+        if (empty($distributeur)) {
+            return response()->json(
+                ['error' => 'this distributor does not exist'],
+                404);
+        }
+
+        $distributeur->delete();
     }
 }
