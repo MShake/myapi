c0dee826cce0dec4b401657f2d1394ae934fa51d
lionel95200x <lionel95200@gmail.com>
2016-04-13 10:18:33 +0200

    jwtauth

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-<<<<<<< HEAD
-=======
+
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\JWTAuth;
->>>>>>> b6d50b01ac5b60907a9cddaf28bfdd2ba3947f72
 
 class JWTController extends Controller
 {
@@ -23,11 +21,9 @@ class JWTController extends Controller
             if (! $token = JWTAuth::attempt($credentials)) {
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
-<<<<<<< HEAD
+
         } catch (JWTException $e) {
-=======
-        } catch (JWTExceptioneption $e) {
->>>>>>> b6d50b01ac5b60907a9cddaf28bfdd2ba3947f72
+
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
