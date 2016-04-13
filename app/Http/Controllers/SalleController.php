6ef3c831520dde76db35cbfbc6fee33cd5656cf0
Shake <fidalgo.antoine@gmail.com>
2016-04-13 12:40:33 +0200

    Ajout GET salles & POST salle

--- app/Http/Controllers/SalleController.php
+++ app/Http/Controllers/SalleController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Salle;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class SalleController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @SWG\Get(
+     *     path="/salle",
+     *     summary="Display a listing of salles.",
+     *     tags={"salle"},
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Salle")
+     *          ),
+     *     ),
+     *  )
      */
     public function index()
     {
-        //
+        $salles = Salle::all();
+        return $salles;
     }
 
     /**
@@ -29,14 +42,77 @@ class SalleController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @SWG\Post(
+     *     path="/salle",
+     *     summary="Create a salle",
+     *     description="Use this method to create a salle",
+     *     operationId="createSalle",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"salle"},
+     *     @SWG\Parameter(
+     *         description="Numero de la salle",
+     *         in="formData",
+     *         name="numero_salle",
+     *         type="integer",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Nom de la salle",
+     *         in="formData",
+     *         name="nom_salle",
+     *         required=true,
+     *         type="string",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Etage de la salle",
+     *         in="formData",
+     *         name="etage_salle",
+     *         type="integer",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Nombre de places de la salle",
+     *         in="formData",
+     *         name="places",
+     *         type="integer",
+     *         format="string"
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="Salle created"
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Champs manquant obligatoire ou incorrect"
+     *     )
+     * )
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'numero_salle' => 'unique:salles',
+            'nom_salle' => 'unique:salles',
+            'etage_salle' => 'max:255',
+            'places' => ''
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+        $salle = new Salle;
+        $salle->numero_salle = $request->numero_salle;
+        $salle->nom_salle = $request->nom_salle;
+        $salle->etage_salle = $request->etage_salle;
+        $salle->places = $request->places;
+        $salle->save();
+
+        return response()->json(
+            $salle,
+            201);
     }
 
     /**
--- app/Salle.php
+++ app/Salle.php
@@ -4,7 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *     required={"id_salle", "nom_salle"},
+ *     @SWG\Xml(name="Salle"),
+ *     @SWG\Property(property="id_salle", format="int64", type="integer", default=62),
+ *     @SWG\Property(property="numero_salle", format="int64", type="integer", default=1),
+ *     @SWG\Property(property="nom_salle", format="string", type="string", default="Kirk Hammett"),
+ *     @SWG\Property(property="etage_salle", format="int64", type="integer", default=0),
+ *     @SWG\Property(property="places", format="int64", type="integer", default=130),
+ * )
+ */
+
 class Salle extends Model
 {
-    //
+    public $primaryKey = "id_salle";
+    public $timestamps = false;
 }
