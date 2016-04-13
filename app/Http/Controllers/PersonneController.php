260165867479c48033da0f7d6793d11f709649d4
Kalimerre <kalimerre@gmail.com>
2016-04-13 13:46:48 +0200

    CRUD Personne

--- app/Http/Controllers/PersonneController.php
+++ app/Http/Controllers/PersonneController.php
@@ -120,7 +120,7 @@ class PersonneController extends Controller
             'date_naissance' => 'required|date',
             'email' => 'required|max:255',
             'adresse' => 'max:255',
-            'cpostal' => 'numeric|max:255|required',
+            'cpostal' => 'numeric|required',
             'ville' => 'max:255|required',
             'pays' => 'required|max:255'
         ]);
@@ -192,16 +192,142 @@ class PersonneController extends Controller
     }
 
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     /**
+     * @SWG\Put(
+     *     path="/personne/{id_personne}",
+     *     summary="Update a personne",
+     *     description="Use this method to update a personne",
+     *     operationId="updatePersonne",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"personne"},
+     *     @SWG\Parameter(
+     *         description="ID of personne to update",
+     *         in="path",
+     *         name="id_personne",
+     *         required=true,
+     *         type="integer",
+     *         format="int64"
+     *     ),
+      *      @SWG\Parameter(
+      *         description="Lastname",
+      *         in="formData",
+      *         name="nom",
+      *         type="string",
+      *         maximum="255",
+      *         required=true
+      *     ),
+      *      @SWG\Parameter(
+      *         description="Firstname",
+      *         in="formData",
+      *         name="prenom",
+      *         required=true,
+      *         type="string",
+      *         maximum="255"
+      *     ),
+      *     @SWG\Parameter(
+      *         description="Birthdate",
+      *         in="formData",
+      *         name="date_naissance",
+      *         required=true,
+      *         type="string",
+      *         format="date"
+      *     ),
+      *     @SWG\Parameter(
+      *         description="E-mail",
+      *         in="formData",
+      *         name="email",
+      *         type="string",
+      *         maximum="255",
+      *         required=true
+      *     ),
+      *     @SWG\Parameter(
+      *         description="Address",
+      *         in="formData",
+      *         name="adresse",
+      *         type="string",
+      *         maximum="255"
+      *     ),
+      *     @SWG\Parameter(
+      *         description="ZIP Code",
+      *         in="formData",
+      *         name="cpostal",
+      *         type="integer",
+      *         maximum="255",
+      *         required=true
+      *     ),
+      *     @SWG\Parameter(
+      *         description="City",
+      *         in="formData",
+      *         name="ville",
+      *         type="string",
+      *         maximum="255",
+      *         required=true
+      *     ),
+      *     @SWG\Parameter(
+      *         description="Country",
+      *         in="formData",
+      *         name="pays",
+      *         type="string",
+      *         maximum="255",
+      *         required=true
+      *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Film updated"
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Film not found"
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Champs incorrect"
+     *     )
+     * )
      */
     public function update(Request $request, $id)
     {
-        //
+        $personne = Personne::find($id);
+
+        if (empty($personne)) {
+            return response()->json(
+                ['error' => 'this personne does not exist'],
+                404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nom' => 'required|max:255',
+            'prenom' => 'required|max:255',
+            'date_naissance' => 'required|date',
+            'email' => 'required|max:255',
+            'adresse' => 'max:255',
+            'cpostal' => 'numeric|required',
+            'ville' => 'max:255|required',
+            'pays' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+
+        $personne->nom = $request->nom != null ? $request->nom : $personne->nom;
+        $personne->prenom = $request->prenom != null ? $request->prenom : $personne->prenom;
+        $personne->date_naissance = $request->date_naissance != null ? $request->date_naissance : $personne->date_naissance;
+        $personne->email = $request->email != null ? $request->email : $personne->email;
+        $personne->adresse = $request->adresse != null ? $request->adresse : $personne->adresse;
+        $personne->cpostal = $request->cpostal != null ? $request->cpostal : $personne->cpostal;
+        $personne->ville = $request->ville != null ? $request->ville : $personne->ville;
+        $personne->pays = $request->pays != null ? $request->pays : $personne->pays;
+
+        $personne->save();
+
+        return response()->json(
+            $personne,
+            200
+        );
     }
 
     /**
