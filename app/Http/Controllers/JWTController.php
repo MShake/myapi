5a587aa777cf94d145ab89cff028c6869bdd20a4
Kalimerre <kalimerre@gmail.com>
2016-04-13 16:26:40 +0200

    Authenticate : done, generate token ok

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -12,6 +12,45 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWTController extends Controller
 {
+
+    /**
+     * @SWG\Post(
+     *     path="/authenticate",
+     *     summary="Create a token",
+     *     description="Use this method to create a token",
+     *     operationId="createAuthenticate",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"authenticate"},
+     *      @SWG\Parameter(
+     *         description="E-mail",
+     *         in="formData",
+     *         name="email",
+     *         type="string",
+     *         required=true,
+     *         maximum="255"
+     *     ),
+     *      @SWG\Parameter(
+     *         description="Password",
+     *         in="formData",
+     *         name="password",
+     *         type="string",
+     *         required=true,
+     *         maximum="255"
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="Token created"
+     *     ),
+     *     @SWG\Response(
+     *         response=401,
+     *         description="Invalid Credentials"
+     *     ),
+     *     @SWG\Response(
+     *         response=500,
+     *         description="Could not create token"
+     *     )
+     * )
+     */
     public function authenticate(Request $request)
     {
         // grab credentials from the request
