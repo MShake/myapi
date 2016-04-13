d9c2aa2456b16505c25ff0123d1cb1a5fee962a1
Kalimerre <kalimerre@gmail.com>
2016-04-13 12:43:35 +0200

    Add initial commit for PersonneController

--- app/Http/Controllers/PersonneController.php
+++ app/Http/Controllers/PersonneController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Personne;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class PersonneController extends Controller
 {
@@ -13,30 +15,142 @@ class PersonneController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *     path="/personne",
+     *     summary="Display a listing of personnes.",
+     *     tags={"personne"},
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Personne")
+     *          ),
+     *     ),
+     *  )
+     */
     public function index()
     {
-        //
+        $personnes = Personne::all();
+        return $personnes;
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @SWG\Post(
+     *     path="/personne",
+     *     summary="Create a personne",
+     *     description="Use this method to create a personne",
+     *     operationId="createPersonne",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"personne"},
+     *      @SWG\Parameter(
+     *         description="Lastname",
+     *         in="formData",
+     *         name="nom",
+     *         type="string",
+     *         maximum="255",
+     *         required=true
+     *     ),
+     *      @SWG\Parameter(
+     *         description="Firstname",
+     *         in="formData",
+     *         name="prenom",
+     *         required=true,
+     *         type="string",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Birthdate",
+     *         in="formData",
+     *         name="date_naissance",
+     *         required=true,
+     *         type="string",
+     *         format="date"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="E-mail",
+     *         in="formData",
+     *         name="email",
+     *         type="string",
+     *         maximum="255",
+     *         required=true
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Address",
+     *         in="formData",
+     *         name="adresse",
+     *         type="string",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ZIP Code",
+     *         in="formData",
+     *         name="cpostal",
+     *         type="integer",
+     *         maximum="255",
+     *         required=true
+     *     ),
+     *     @SWG\Parameter(
+     *         description="City",
+     *         in="formData",
+     *         name="ville",
+     *         type="string",
+     *         maximum="255",
+     *         required=true
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Country",
+     *         in="formData",
+     *         name="pays",
+     *         type="string",
+     *         maximum="255",
+     *         required=true
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="Personne Created"
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Missing field or incorrect syntax. Check the error message"
+     *     )
+     * )
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nom' => 'required|max:255',
+            'prenom' => 'required|max:255',
+            'date_naissance' => 'required|date',
+            'email' => 'required|max:255',
+            'adresse' => 'max:255',
+            'cpostal' => 'numeric|max:255|required',
+            'ville' => 'max:255|required',
+            'pays' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+        $personne = new Personne();
+        $personne->nom = $request->nom;
+        $personne->prenom = $request->prenom;
+        $personne->date_naissance = $request->date_naissance;
+        $personne->email = $request->email;
+        $personne->adresse = $request->adresse;
+        $personne->cpostal = $request->cpostal;
+        $personne->ville = $request->ville;
+        $personne->pays = $request->pays;
+        $personne->save();
+
+        return response()->json(
+            $personne,
+            201);
     }
 
     /**
@@ -50,16 +164,6 @@ class PersonneController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
--- app/Personne.php
+++ app/Personne.php
@@ -4,7 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *     required={"id_personne", "nom", "prenom", "date_naissance", "email", "cpostal", "ville", "pays"},
+ *     @SWG\Xml(name="Personne"),
+ *     @SWG\Property(property="id_personne", format="int64", type="integer", default=42),
+ *     @SWG\Property(property="nom", format="string", type="string", default="Panini"),
+ *     @SWG\Property(property="prenom", format="string", type="string", default="Flourian"),
+ *     @SWG\Property(property="date_naissance", format="date", type="string", default="2016-06-15"),
+ *     @SWG\Property(property="email", format="string", type="string", default="panini@esgi.fr"),
+ *     @SWG\Property(property="adresse", format="string", type="string", default="242 rue du Faubourg Saint Antoine"),
+ *     @SWG\Property(property="cpostal", format="int", type="integer", default=75012),
+ *     @SWG\Property(property="ville", format="string", type="string", default="Paris"),
+ *     @SWG\Property(property="pays", format="string", type="string", default="France")
+ * )
+ */
+
 class Personne extends Model
 {
-    //
+    public $primaryKey = "id_personne";
+    public $timestamps = false;
 }
