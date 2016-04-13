33062826f3f622946ca693b24f9ea9959cf06db7
Shake <fidalgo.antoine@gmail.com>
2016-04-13 14:06:13 +0200

    Maj PUT Salle et MAJ Employe

--- app/Http/Controllers/EmployeController.php
+++ app/Http/Controllers/EmployeController.php
@@ -55,7 +55,7 @@ class EmployeController extends Controller
      *     ),
      *     @SWG\Response(
      *         response=201,
-     *         description="Film created"
+     *         description="Employe created"
      *     ),
      *     @SWG\Response(
      *         response=422,
@@ -100,15 +100,73 @@ class EmployeController extends Controller
 
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @SWG\Put(
+     *     path="/employe/{id_employe}",
+     *     summary="Update a employe",
+     *     description="Use this method to update a employe",
+     *     operationId="updateEmploye",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"employe"},
+     *     @SWG\Parameter(
+     *         description="ID of employe to return",
+     *         in="path",
+     *         name="id_employe",
+     *         required=true,
+     *         type="integer",
+     *         format="int64"
+     *     ),
+     *      @SWG\Parameter(
+     *         description="id de la personne (id)",
+     *         in="formData",
+     *         name="id_personne",
+     *         type="integer",
+     *         maximum="255"
+     *     ),
+     *      @SWG\Parameter(
+     *         description="Fonction d'une personne (id)",
+     *         in="formData",
+     *         name="id_fonction",
+     *         type="integer",
+     *         maximum="255"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description=" Employe updated"
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Champs manquant obligatoire ou incorrect"
+     *     )
+     * )
      */
     public function update(Request $request, $id)
     {
-        //
+        $employe = Employe::find($id);
+
+        if (empty($employe)) {
+            return response()->json(
+                ['error' => 'this employe does not exist'],
+                404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'id_personne' => 'exists:personnes,id_personne',
+            'id_fonction' => 'exists:fonctions,id_fonction',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+        $employe->id_personne = $request->id_personne != null ? $request->id_personne : $employe->id_personne;
+        $employe->id_fonction = $request->id_fonction != null ? $request->id_fonction : $employe->id_fonction;
+        $employe->save();
+
+        return response()->json(
+            $employe,
+            200);
     }
 
     /**
